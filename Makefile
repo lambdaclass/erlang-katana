02f5ec75874a837e8543966208fe58c5471da8de
Juan Facorro <juan.facorro@gmail.com>
2014-08-12 14:21:28 -0300

    [#1] get/[2,3] to access values in nested maps.

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
 PROJECT = katana
+
+DEPS = sync
+
+dep_sync = git https://github.com/rustyio/sync.git master
+
 include erlang.mk
+
+shell: app
+	erl -pa ebin -pa deps/*/ebin -s sync
---
+++ src/katana_maps.erl
@@ -0,0 +1,28 @@
+-module(katana_maps).
+
+-export([
+         get/2,
+         get/3
+        ]).
+
+-spec get(term(), map()) -> term().
+get(Keys, Map) ->
+    get(Keys, Map, undefined).
+
+-spec get(term(), map(), term()) -> term().
+get([Key], Map, Default) ->
+    get(Key, Map, Default);
+get([Key | Rest], Map, Default) ->
+    case get(Key, Map, Default) of
+        NewMap when is_map(NewMap) ->
+            get(Rest, NewMap, Default);
+        _ ->
+            Default
+    end;
+get(Key, Map, Default) ->
+    case maps:is_key(Key, Map) of
+        true ->
+            maps:get(Key, Map);
+        false ->
+            Default
+    end.
