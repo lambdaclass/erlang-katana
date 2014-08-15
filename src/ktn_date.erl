753820bea52b496d4604c74598603d458befd436
Federico Carrone <federico.carrone@gmail.com>
2014-08-15 15:34:41 -0300

    commas and spaces

--- src/ktn_date.erl
+++ src/ktn_date.erl
@@ -7,7 +7,7 @@
 %% @doc Returns the current date in a human readable format binary.
 -spec now_human_readable() -> binary().
 now_human_readable() ->
-    TimeStamp = {_,_,Micro} = os:timestamp(),
+    TimeStamp = {_, _, Micro} = os:timestamp(),
     {{Year, Month, Day},
      {Hour, Minute, Second}} = calendar:now_to_universal_time(TimeStamp),
     DateList = io_lib:format("~p-~p-~pT~p:~p:~p.~6..0wZ",
