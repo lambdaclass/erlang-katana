59aab763afb1fb850cc120c960b520dd9b69538e
Juan Facorro <juan.facorro@gmail.com>
2015-02-13 15:32:38 -0300

    [#24] Added ktn_code:consult/1 and moved split_when/2 to ktn_lists.

--- src/ktn_lists.erl
+++ src/ktn_lists.erl
@@ -1,7 +1,8 @@
 -module(ktn_lists).
 
 -export([
-         delete_first/2
+         delete_first/2,
+         split_when/2
         ]).
 
 %% @doc Returns a copy of List deleting the first Element where Fun(Element)
@@ -9,14 +10,33 @@
 %% @end
 -spec delete_first(fun((term()) -> boolean()), list()) -> list().
 delete_first(Fun, List) ->
-  delete_first(Fun, List, []).
+    delete_first(Fun, List, []).
 
 delete_first(Fun, [], Acc) when is_function(Fun, 1) ->
-  lists:reverse(Acc);
+    lists:reverse(Acc);
 delete_first(Fun, [Head | Tail], Acc) ->
-  case Fun(Head) of
-    false ->
-      delete_first(Fun, Tail, [Head | Acc]);
-    true -> 
-      lists:concat([lists:reverse(Acc), Tail])
-  end.
+    case Fun(Head) of
+        false ->
+            delete_first(Fun, Tail, [Head | Acc]);
+        true ->
+            lists:concat([lists:reverse(Acc), Tail])
+    end.
+
+-spec split_when(fun(), list()) -> list().
+split_when(When, List) ->
+    split_when(When, List, [[]]).
+
+split_when(When, [], [[] | Results]) ->
+    split_when(When, [], Results);
+split_when(_When, [], Results) ->
+    Reversed = lists:map(fun lists:reverse/1, Results),
+    lists:reverse(Reversed);
+split_when(When, [Head | Tail], [Current0 | Rest]) ->
+    Current = [Head | Current0],
+    Result = case When(Head) of
+                 true ->
+                     [[], Current | Rest];
+                 false ->
+                     [Current | Rest]
+             end,
+    split_when(When, Tail, Result).
