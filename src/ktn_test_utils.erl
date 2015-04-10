d3de2c649f961f5e5c1dcca74046511f268ece10
Iñaki Garay <igarai@gmail.com>
2015-04-10 11:50:03 -0300

    Less generic names

--- src/ktn_test_utils.erl
+++ src/ktn_test_utils.erl
@@ -1,26 +1,26 @@
 -module(ktn_test_utils).
 
 -export(
-  [ assert/4
-  , test/4
+  [ assert_response/4
+  , test_response/4
   ]).
 
 -type test_subject() :: status | headers | body.
 -type match_type()   :: exact | partial.
 
--spec assert(test_subject(), match_type(), map(), chtc:response()) -> ok.
-assert(Test, MatchType, Params, Response) ->
-  ok = test(Test, MatchType, Params, Response).
+-spec assert_response(test_subject(), match_type(), map(), chtc:response()) -> ok.
+assert_response(Test, MatchType, Params, Response) ->
+  ok = test_response(Test, MatchType, Params, Response).
 
--spec test(test_subject(), match_type(), term(), term()) ->
+-spec test_response(test_subject(), match_type(), term(), term()) ->
   ok | {error, term()}.
-test(status, partial, [C, $?, $?], Response) ->
+test_response(status, partial, [C, $?, $?], Response) ->
   Pattern = [C] ++ "[0-9][0-9]",
-  test(status, partial, Pattern, Response);
-test(status, partial, [C1, C2, $?], Response) ->
+  test_response(status, partial, Pattern, Response);
+test_response(status, partial, [C1, C2, $?], Response) ->
   Pattern = [C1, C2] ++ "[0-9]",
-  test(status, partial, Pattern, Response);
-test(status, partial, Pattern, Response) ->
+  test_response(status, partial, Pattern, Response);
+test_response(status, partial, Pattern, Response) ->
   #{status := Status} = Response,
   ResStatus = ktn_strings:to_string(Status),
   case re:compile(Pattern) of
@@ -33,7 +33,7 @@ test(status, partial, Pattern, Response) ->
     {error, Error} ->
       {error, {regex_compile_fail, Error}}
   end;
-test(status, exact, Status, Response) ->
+test_response(status, exact, Status, Response) ->
   #{status := ResStatus} = Response,
   StatusStr    = ktn_strings:to_string(Status),
   ResStatusStr = ktn_strings:to_string(ResStatus),
@@ -41,7 +41,7 @@ test(status, exact, Status, Response) ->
     StatusStr -> ok;
     _Other    -> {error, {nomatch, StatusStr, ResStatusStr}}
   end;
-test(headers, partial, Headers, Response) ->
+test_response(headers, partial, Headers, Response) ->
   #{headers := ResHeaders} = Response,
   HeadersNorm =
     [{string:to_lower(X), string:to_lower(Y)} || {X, Y} <- Headers],
@@ -51,7 +51,7 @@ test(headers, partial, Headers, Response) ->
     []             -> ok;
     MissingHeaders -> {error, {missing_headers, MissingHeaders, ResHeadersNorm}}
   end;
-test(headers, exact, Headers, Response) ->
+test_response(headers, exact, Headers, Response) ->
   #{headers := ResHeaders} = Response,
   HeadersNorm    =
     [{string:to_lower(X), string:to_lower(Y)} || {X, Y} <- Headers],
@@ -61,7 +61,7 @@ test(headers, exact, Headers, Response) ->
     {[], []} -> ok;
     _        -> {error, {nomatch, HeadersNorm, ResHeadersNorm}}
   end;
-test(body, partial, Pattern, Response) ->
+test_response(body, partial, Pattern, Response) ->
   #{body := ResBody} = Response,
   ResBodyStr = ktn_strings:to_string(ResBody),
   case re:compile(Pattern) of
@@ -74,7 +74,7 @@ test(body, partial, Pattern, Response) ->
     {error, Error} ->
       {error, {regex_compile_fail, Error}}
   end;
-test(body, exact, Text, Response) ->
+test_response(body, exact, Text, Response) ->
   #{body := ResBody} = Response,
   ResBodyStr = ktn_strings:to_string(ResBody),
   Body       = ktn_strings:to_string(Text),
